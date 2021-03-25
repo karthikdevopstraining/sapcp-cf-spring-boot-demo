6ff639f1f552a5e5546aac9aa94238208546bc8f
karthik-2594 <61648106+karthik-2594@users.noreply.github.com>
2021-03-25 13:42:17 +0530

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Sahil Bawa! Subscribe, Like, Share!";
+		return "This is NJR SAP DEMO";
 	}
 
 }
